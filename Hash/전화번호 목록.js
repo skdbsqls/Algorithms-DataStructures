5d9bf76dc35b9585f878c37db92d436c4874d38b
skdbsqls <skdbsqls98@naver.com>
2023-11-27 20:20:58 +0900

    Hash: 전화번호 목록

--- Hash/전화번호 목록.js
+++ Hash/전화번호 목록.js
@@ -11,12 +11,20 @@
 어떤 번호가 다른 번호의 접두어인 경우가 있으면 false를 그렇지 않으면 true를 return 하도록 solution 함수를 작성해주세요.
 
 [제한 사항]
-phone_book의 길이는 1 이상 1,000,000 이하입니다.
-각 전화번호의 길이는 1 이상 20 이하입니다.
-같은 전화번호가 중복해서 들어있지 않습니다.
+- phone_book의 길이는 1 이상 1,000,000 이하입니다.
+- 각 전화번호의 길이는 1 이상 20 이하입니다.
+- 같은 전화번호가 중복해서 들어있지 않습니다.
 */
 
 function solution(phone_book) {
-  var answer = true;
-  return answer;
+  // 전화번호 배열 정렬
+  phone_book.sort();
+
+  // 현재 번호와 다음 번호를 비교할 것이기 때문에 phone_book.length - 1
+  for (let i = 0; i < phone_book.length - 1; i++) {
+    // 현재 번호가 다음 번호의 시작과 같으면 false
+    if (phone_book[i] === phone_book[i + 1].slice(0, phone_book[i].length))
+      return false;
+  }
+  return true;
 }
