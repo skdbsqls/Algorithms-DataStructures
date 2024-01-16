a69965fada649a04ee80f5e56bcdde4a8467da04
skdbsqls <skdbsqls98@naver.com>
2024-01-17 00:39:16 +0900

    완전탐색: 소수찾기

--- 완전탐색/소수찾기.js
+++ 완전탐색/소수찾기.js
@@ -12,44 +12,46 @@
 - "013"은 0, 1, 3 숫자가 적힌 종이 조각이 흩어져있다는 의미입니다.
 */
 
-// 소수 판별하기
+// (1) 소수 판별하기
 const isPrime = (num) => {
-  // 0과 1은 소수가 아님
+  // 음수 그리고 0과 1은 소수가 아님
   if (num < 2) return false;
-  // 소수는 2부터 1과 자신만으로 나누어 떨어짐
+  // 소수는 2부터 √n까지 숫자 중 나누어 떨어지는 숫자가 있어선 안 됨
   for (let i = 2; i <= Math.sqrt(num); i++) {
     if (num % i === 0) return false;
   }
   return true;
 };
 
+// (2) 모든 순열 구하기
+const getPermutation = (set, arr, fixed) => {
+  if (arr.length) {
+    for (let i = 0; i < arr.length; i++) {
+      let temp = [...arr];
+      // fixed를 제외한 배열 만들기
+      let rest = temp.splice(i, 1);
+      // fixed 바꿔주기
+      let newFixed = fixed + arr[i];
+
+      // 소수 판별, parseInt? "011"과 "11"은 같은 숫자로 취급하기 위함
+      if (isPrime(parseInt(newFixed))) {
+        set.add(parseInt(newFixed));
+      }
+
+      getPermutation(set, rest, newFixed);
+    }
+  }
+};
+
+// 정답 구하기
 function solution(numbers) {
   // 배열로 변환
   const arr = numbers.split("");
-  const results = [];
+  // Set을 통해 중복 제거
+  const set = new Set();
 
-  // 모든 순열 구하기
-  const getPermutation = (arr, fixed) => {
-    if (arr.length) {
-      for (let i = 0; i < arr.length; i++) {
-        // fixed을 제외한 배열 만들기
-        const temp = [...arr];
-        temp.splice(i, 1);
+  getPermutation(set, arr, "");
 
-        // parseInt? "011"과 "11"은 같은 숫자로 취급
-        if (isPrime(parseInt(fixed + arr[i]))) {
-          results.push(parseInt(fixed + arr[i]));
-        }
-        getPermutation(temp, fixed + arr[i]);
-      }
-    }
-  };
-  getPermutation(arr, "");
-
-  console.log(results);
-
-  // 중복 제거
-  const set = new Set(results);
   return set.size;
 }
 
@@ -80,12 +82,12 @@ const isPrime3 = (n) => {
 
 // (4) 에라토스테네스의 체
 const isPrime4 = (n) => {
-  // 인덱스가 0부터 n까지인 배열 생성 (인덱스 0과 1은 false 나머지는 true)
+  // 인덱스가 0부터 n까지인 배열 생성 (인덱스 0과 1은 false, 나머지는 true)
   let arr = Array(n + 1)
     .fill(true)
     .fill(false, 0, 2);
 
-  // 2부터 n의 제곱근까지 반복
+  // i는 2부터 n의 제곱근까지만 반복
   for (let i = 2; i * i <= n; i++) {
     if (arr[i]) {
       // i의 배수는 false로 변경
@@ -95,6 +97,8 @@ const isPrime4 = (n) => {
     }
   }
 
-  // n까지의 숫자 중 소수만 true인 배열 반환
+  // 0부터 n까지의 숫자 중 소수만 true인 배열 반환
   return arr;
 };
+
+// 순열과 조합
