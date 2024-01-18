e0e7d879f6730dad2ed2a39a5673f7ffbb808980
skdbsqls <skdbsqls98@naver.com>
2024-01-18 19:08:37 +0900

    완전탐색: 카펫

--- 완전탐색/카펫.js
+++ 완전탐색/카펫.js
@@ -10,6 +10,7 @@ Leo가 본 카펫에서 갈색 격자의 수 brown, 노란색 격자의 수 yell
 - 카펫의 가로 길이는 세로 길이와 같거나, 세로 길이보다 깁니다.
 */
 
+// 풀이 1
 function solution(brown, yellow) {
   let answer = [];
 
@@ -25,18 +26,18 @@ function solution(brown, yellow) {
   // 약수의 가장 가운데 값 구하기
   const middle = Math.floor(measures.length / 2);
 
-  // 약수의 개수가 홀수 일 때
+  // 약수의 개수가 홀수일 때
   if (measures.length % 2 !== 0) {
     answer.push(measures[middle]);
     answer.push(measures[middle]);
   }
-  // 약수의 개수가 짝수 일 때
+  // 약수의 개수가 짝수일 때
   else {
     // 약수의 짝을 맞추기 위해 배열을 둘로 나눔
     const arr1 = measures.slice(0, middle);
     const arr2 = measures.slice(middle).reverse();
 
-    // 두 배열을 돌면서 약수의 짝 중에 yellow 수가 맞는 것을 찾아줌
+    // 두 배열을 돌면서 약수의 짝들 중에 yellow 수가 맞는 것을 찾아줌
     for (let i = 0; i < middle; i++) {
       if ((arr1[i] - 2) * (arr2[i] - 2) === yellow) {
         answer.push(arr2[i]);
@@ -45,5 +46,22 @@ function solution(brown, yellow) {
     }
   }
 
+  return answer;
+}
+
+// 풀이 2
+function solution(brown, yellow) {
+  let answer = [];
+  // x는 가로, y는 세로 (x와 y의 최소값은 3)
+  for (let y = 3; y <= (brown + yellow) / y; y++) {
+    // x(가로) = (brown + yellow) / y(세로)
+    let x = Math.floor((brown + yellow) / y);
+    // x, y가 될 수 있는 수 중에서 yellow 수가 맞는 것 찾기
+    if ((x - 2) * (y - 2) === yellow) {
+      answer.push(x);
+      answer.push(y);
+      break;
+    }
+  }
   return answer;
 }
