de4fa8f430cca074e3da4fa8275248d2a6345509
skdbsqls <skdbsqls98@naver.com>
2024-01-24 01:57:28 +0900

    완전탐색: 모음사전

--- 완전탐색/모음사전.js
+++ 완전탐색/모음사전.js
@@ -11,6 +11,22 @@
 */
 
 function solution(word) {
-  var answer = 0;
-  return answer;
+  let vowels = ["A", "E", "I", "O", "U"];
+  let dictionary = [];
+
+  const dfs = (cur, len) => {
+    if (len > 5) return;
+
+    dictionary.push(cur);
+
+    for (let i = 0; i < vowels.length; i++) {
+      dfs(cur + vowels[i], len + 1);
+    }
+  };
+
+  dfs("", 0);
+
+  return dictionary.indexOf(word);
 }
+
+solution("A");
