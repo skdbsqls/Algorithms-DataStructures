9b3ef2abba8b8f8a0327a26f649ee279a6c28961
skdbsqls <skdbsqls98@naver.com>
2024-01-25 01:53:56 +0900

    완전탐색: 모음사전

--- 완전탐색/모음사전.js
+++ 완전탐색/모음사전.js
@@ -55,3 +55,16 @@ function solution(word) {
 
   return count;
 }
+
+// 풀이 3
+function solution(word) {
+  let answer = 0;
+  const vowels = ["A", "E", "I", "O", "U"];
+  const counts = [781, 156, 31, 6, 1];
+
+  for (let i = 0; i < word.length; i++) {
+    answer += vowels.indexOf(word[i]) * counts[i] + 1;
+  }
+
+  return answer;
+}
