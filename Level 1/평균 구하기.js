e0086a9ef5675d6a9fb01d41ab6de9ae0e0b5405
skdbsqls <skdbsqls98@naver.com>
2024-02-07 02:56:13 +0900

    Lv.1: 평균 구하기

--- Level 1/평균 구하기.js
+++ Level 1/평균 구하기.js
@@ -8,6 +8,11 @@
 */
 
 function solution(arr) {
-  var answer = 0;
-  return answer;
+  let answer = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    answer += arr[i];
+  }
+
+  return answer / arr.length;
 }
