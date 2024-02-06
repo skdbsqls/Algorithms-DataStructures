fd82d383fba839b80eb9796d6e8c35207ca5285b
skdbsqls <skdbsqls98@naver.com>
2024-02-06 20:09:30 +0900

    Lv.1: 나머지가 1이 되는 수 찾기

--- Level 1/나머지가 1이 되는 수 찾기.js
+++ Level 1/나머지가 1이 되는 수 찾기.js
@@ -9,6 +9,14 @@ n을 x로 나눈 나머지가 1이 되도록 하는 가장 작은 자연수 x를
 */
 
 function solution(n) {
-  var answer = 0;
+  let answer = 0;
+
+  for (let i = 1; i <= n; i++) {
+    if (n % i === 1) {
+      answer = i;
+      break; // 가장 작은 수를 구해야 함으로 처음 i를 구한 후 break
+    }
+  }
+
   return answer;
 }
