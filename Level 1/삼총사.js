a8dbf9f390f6630338fc8539a5dffd3ef3edf486
skdbsqls <skdbsqls98@naver.com>
2024-02-15 21:07:52 +0900

    Lv 1: 삼총사

--- Level 1/삼총사.js
+++ Level 1/삼총사.js
@@ -63,3 +63,18 @@ function solution(number) {
   combination([], 0);
   return count;
 }
+
+// 풀이 3
+function solution(number) {
+  let answer = 0;
+  for (let i = 0; i < number.length - 2; i++) {
+    for (let j = i + 1; j < number.length - 1; j++) {
+      for (let k = j + 1; k < number.length; k++) {
+        if (number[i] + number[j] + number[k] === 0) {
+          answer++;
+        }
+      }
+    }
+  }
+  return answer;
+}
