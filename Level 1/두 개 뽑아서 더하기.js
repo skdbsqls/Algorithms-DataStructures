f1188b5455668ffc4fcdad4d2313fe9125992e81
skdbsqls <skdbsqls98@naver.com>
2024-02-20 15:41:54 +0900

    Lv 1: 두 개 뽑아서 더하기

---
+++ Level 1/두 개 뽑아서 더하기.js
@@ -0,0 +1,26 @@
+/*
+[문제 설명]
+정수 배열 numbers가 주어집니다. 
+numbers에서 서로 다른 인덱스에 있는 두 개의 수를 뽑아 더해서 만들 수 있는 모든 수를 배열에 오름차순으로 담아 return 하도록 solution 함수를 완성해주세요.
+
+[제한사항]
+- numbers의 길이는 2 이상 100 이하입니다.
+- numbers의 모든 수는 0 이상 100 이하입니다.
+*/
+
+function solution(numbers) {
+  let sums = []; // 두 개의 요소의 합
+
+  // 두 개의 요소의 모든 합
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      let sum = numbers[i] + numbers[j];
+      sums.push(sum);
+    }
+  }
+
+  // 중복 제거하기
+  let set = new Set([...sums]);
+
+  return [...set].sort((a, b) => a - b); // 오름차순 정렬
+}
