d41181b4f4b6957dd14299760f1c7f055b42f633
skdbsqls <skdbsqls98@naver.com>
2024-02-26 00:23:46 +0900

    Lv 1: 2016년

--- Level 1/2016년.js
+++ Level 1/2016년.js
@@ -19,3 +19,17 @@ function solution(a, b) {
 
   return week[day];
 }
+
+// 다른 풀이
+function solution(a, b) {
+  const month = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  const week = ["THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED"];
+
+  let days = 0;
+  for (let i = 0; i < a - 1; i++) {
+    days += month[i];
+  }
+  days += b;
+
+  return week[days % 7];
+}
