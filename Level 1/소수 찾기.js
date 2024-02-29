83f1c81bbe693dc3418c12875801d910a04dcb3b
skdbsqls <skdbsqls98@naver.com>
2024-03-01 02:02:31 +0900

    Lv1 소수 찾기

---
+++ Level 1/소수 찾기.js
@@ -0,0 +1,29 @@
+/*
+[문제 설명]
+1부터 입력받은 숫자 n 사이에 있는 소수의 개수를 반환하는 함수, solution을 만들어 보세요.
+
+소수는 1과 자기 자신으로만 나누어지는 수를 의미합니다.
+(1은 소수가 아닙니다.)
+
+[제한 조건]
+- n은 2이상 1000000이하의 자연수입니다.
+*/
+
+function solution(n) {
+  // 인덱스가 0부터 n까지인 배열 생성 (인덱스 0과 1은 false, 나머지는 true)
+  let arr = Array(n + 1)
+    .fill(true)
+    .fill(false, 0, 2);
+
+  // i는 2부터 n의 제곱근까지만 반복
+  for (let i = 2; i * i <= n; i++) {
+    if (arr[i]) {
+      // i의 배수는 false로 변경
+      for (let j = i * i; j <= n; j += i) {
+        arr[j] = false;
+      }
+    }
+  }
+
+  return arr.filter((e) => e).length; // 소수의 개수
+}
