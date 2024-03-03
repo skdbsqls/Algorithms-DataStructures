925f7ac0b27e36ff364f7f5a410029c8f2ae6cc7
skdbsqls <skdbsqls98@naver.com>
2024-03-03 20:05:23 +0900

    Lv1 실패율

--- Level 1/실패율.js
+++ Level 1/실패율.js
@@ -62,3 +62,18 @@ function solution(N, stages) {
 
   return answer;
 }
+
+// 다른 풀이
+function solution(N, stages) {
+  let rates = []; // 실패율 배열
+
+  for (let i = 1; i <= N; i++) {
+    let players = stages.filter((x) => x >= i).length; // 스테이지에 도달한 플레이어 수
+    let fails = stages.filter((x) => x === i).length; // 스테이지에 도달했으나 아직 클리어하지 못한 플레이어의 수
+    rates.push([i, fails / players]); // 실패율
+  }
+
+  rates.sort((a, b) => b[1] - a[1]); // 내림차순 정렬
+
+  return rates.map((x) => x[0]);
+}
