4105433fc868a0f42a48caf7f29a18b2e0f6604f
skdbsqls <skdbsqls98@naver.com>
2024-04-08 21:11:50 +0900

    Lv2 짝지어 제거하기

--- Level 2/짝지어 제거하기.js
+++ Level 2/짝지어 제거하기.js
@@ -17,10 +17,15 @@
 */
 
 function solution(s) {
-  var answer = -1;
+  let stack = [];
 
-  // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-  console.log("Hello Javascript");
+  for (let i = 0; i < s.length; i++) {
+    if (stack[stack.length - 1] === s[i]) {
+      stack.pop();
+    } else stack.push(s[i]);
+  }
 
-  return answer;
+  if (stack.length) {
+    return 0;
+  } else return 1;
 }
