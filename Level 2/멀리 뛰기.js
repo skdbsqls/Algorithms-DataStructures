93dea6446b644a41a18544f7d4b9c244c23b4d35
skdbsqls <skdbsqls98@naver.com>
2024-04-14 13:39:52 +0900

    Lv2 멀리 뛰기

--- Level 2/멀리 뛰기.js
+++ Level 2/멀리 뛰기.js
@@ -16,6 +16,14 @@
 */
 
 function solution(n) {
-  var answer = 0;
-  return answer;
+  let dp = new Array(n + 1);
+
+  dp[1] = 1;
+  dp[2] = 2;
+
+  for (let i = 3; i <= n; i++) {
+    dp[i] = (dp[i - 2] + dp[i - 1]) % 1234567;
+  }
+
+  return dp[n];
 }
