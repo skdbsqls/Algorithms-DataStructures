8dc67e7a400557fcccf5a776e151010819800444
skdbsqls <skdbsqls98@naver.com>
2024-05-08 23:10:40 +0900

    Lv2 행렬의 곱셈

--- Level 2/행렬의 곱셈.js
+++ Level 2/행렬의 곱셈.js
@@ -9,6 +9,18 @@
 */
 
 function solution(arr1, arr2) {
-  var answer = [[]];
-  return answer;
+  const arr = [];
+
+  for (let i = 0; i < arr1.length; i++) {
+    let result = [];
+    for (let j = 0; j < arr2[0].length; j++) {
+      let elem = 0;
+      for (let k = 0; k < arr1[0].length; k++) {
+        elem += arr1[i][k] * arr2[k][j];
+      }
+      result.push(elem);
+    }
+    arr.push(result);
+  }
+  return arr;
 }
