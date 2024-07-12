3855c262f9ed92c0984101e5f8b6a3327fe13f11
skdbsqls <skdbsqls98@naver.com>
2024-07-12 17:27:00 +0900

    Lv2 스킬트리

--- Level 2/스킬트리.js
+++ Level 2/스킬트리.js
@@ -8,7 +8,7 @@
 위 순서에 없는 다른 스킬(힐링 등)은 순서에 상관없이 배울 수 있습니다. 
 따라서 스파크 → 힐링 → 라이트닝 볼트 → 썬더와 같은 스킬트리는 가능하지만, 썬더 → 스파크나 라이트닝 볼트 → 스파크 → 힐링 → 썬더와 같은 스킬트리는 불가능합니다.
 
-선행 스킬 순서 skill과 유저들이 만든 스킬트리1를 담은 배열 skill_trees가 매개변수로 주어질 때, 가능한 스킬트리 개수를 return 하는 solution 함수를 작성해주세요.
+선행 스킬 순서 skill과 유저들이 만든 스킬트리를 담은 배열 skill_trees가 매개변수로 주어질 때, 가능한 스킬트리 개수를 return 하는 solution 함수를 작성해주세요.
 
 [제한 조건]
 - 스킬은 알파벳 대문자로 표기하며, 모든 문자열은 알파벳 대문자로만 이루어져 있습니다.
@@ -20,7 +20,40 @@
  - skill_trees의 원소는 길이가 2 이상 26 이하인 문자열이며, 스킬이 중복해 주어지지 않습니다.
 */
 
+// 오답
 function solution(skill, skill_trees) {
-  var answer = -1;
-  return answer;
+  skill = skill.split(""); // 선행 스킬 순서 배열로 변환
+
+  for (let i = 0; i < skill_trees.length; i++) {
+    let tree = skill_trees[i]; // 스킬 트리
+    let skills = [...skill]; // 스킬 복사
+
+    for (let j = 0; j < tree.length; j++) {
+      // 스킬 트리의 스킬이 선행 스킬 순서와 관련 없으면 pass
+      if (!skill.includes(tree[j])) continue;
+      // 선행 스킬 순서를 지키질 않는 스킬 트리는 불가능
+      if (skills.shift() !== tree[j]) {
+        skill_trees[i] = "X";
+        break; // 선행 스킬을 배우지 않은 경우 뒤는 볼 필요 X
+      }
+      skill_trees[i] = "O"; // 선행 스킬 순서를 지킨 경우
+    }
+  }
+  console.log(skill_trees); // ['O', 'O', 'X', 'X', 'X', 'AEX', 'X', 'X', 'O', 'X']
+  console.log(skill_trees.filter((v) => v === "O").length); // 3
+  return skill_trees.filter((v) => v === "O").length;
 }
+
+// 반례
+solution("CBDK", [
+  "CB",
+  "CXYB",
+  "BD",
+  "AECD",
+  "ABC",
+  "AEX",
+  "CDB",
+  "CBKD",
+  "IJCB",
+  "LMDK",
+]);
